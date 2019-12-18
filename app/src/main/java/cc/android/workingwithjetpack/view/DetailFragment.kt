536768a7ece4b263b7ae3124eed384668eec835a
Anmol <anmol.mehta018@gmail.com>
2019-12-18 11:03:23 +0530

    implementing navigation component of jetpack

---
+++ app/src/main/java/cc/android/workingwithjetpack/view/DetailFragment.kt
@@ -0,0 +1,47 @@
+package cc.android.workingwithjetpack.view
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+import cc.android.workingwithjetpack.R
+import kotlinx.android.synthetic.main.fragment_detail.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class DetailFragment : Fragment() {
+
+    private var dogUuid = 0 ;
+    private var ageUuid = 0 ;
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            dogUuid = DetailFragmentArgs.fromBundle(it).dogUuid
+            ageUuid = DetailFragmentArgs.fromBundle(it).ageUuid
+            textView2.text = "Details : "+dogUuid.toString()+" Age : " +ageUuid
+        }
+        fbtn_list.setOnClickListener {
+            val action = DetailFragmentDirections.actionDetailFragmentToListFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+
+}
